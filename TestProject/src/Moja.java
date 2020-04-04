111e74e451640b48cfed92a0f0c6c312212e1e05
LeiteKaka <viktorbaija1@gmail.com>
2020-04-04 11:41:52 +0200

    brisanje Tvoja klase i mozda Moja klase

--- TestProject/src/Moja.java
+++
@@ -1,9 +0,0 @@
-
-public class Moja {
-
-	public static void main(String[] args) {
-	
-
-	}
-
-}
--- TestProject/src/Tvoja.java
+++
@@ -1,9 +0,0 @@
-
-public class Tvoja {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
